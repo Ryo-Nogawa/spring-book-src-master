b301acaa980ecaf95a20342036665596383ce1a8
Ryo Nogawa <69405100+Ryo-Nogawa@users.noreply.github.com>
2025-03-23 12:33:22 +0900

    hans on

--- 0301-training-di/src/test/java/com/example/training/service/TrainingServiceImplTest.java
+++ 0301-training-di/src/test/java/com/example/training/service/TrainingServiceImplTest.java
@@ -2,15 +2,20 @@ package com.example.training.service;
 
 import java.util.List;
 
+import com.example.training.repository.TrainingRepository;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 import com.example.training.entity.Training;
 
 class TrainingServiceImplTest {
+    TrainingServiceImplTest(){
+
+    }
     @Test
     public void test_findAll() {
-        TrainingService trainingService = null;
+        TrainingRepository trainingRepository = new MockTrainingRepository();
+        TrainingService trainingService = new TrainingServiceImpl(trainingRepository);
 
         List<Training> trainings = trainingService.findAll();
         // 結果の確認
